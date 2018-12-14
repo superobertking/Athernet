894bd0710be7a9682b95eebc5c43bd7238725aab
robertking <superobertking@icloud.com>
2018-12-14 22:40:33 +0800

    proj3 part1 finished & soft link

--- proj3/part1_node1.py
+++ proj3/part1_node1.py
@@ -1,23 +1,28 @@
 import socket
 import sys
+import time
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-server_address = ('localhost', 10000)
+server_address = (sys.argv[1], 10000)
 message = b'This is the message.  It will be repeated.'
 
+REP_TIMES = 10
+
 try:
+	for i in range(REP_TIMES):
+		# Send data
+		print('sending {!r}'.format(message))
+		sent = sock.sendto(message, server_address)
 
-    # Send data
-    print('sending {!r}'.format(message))
-    sent = sock.sendto(message, server_address)
-
-    # Receive response
-    print('waiting to receive')
-    data, server = sock.recvfrom(4096)
-    print('received {!r}'.format(data))
+		# Receive response
+		print('waiting to receive')
+		data, server = sock.recvfrom(4096)
+		print('received {!r}'.format(data))
+		if i != REP_TIMES - 1:
+			time.sleep(1)
 
 finally:
-    print('closing socket')
-    sock.close()
+	print('closing socket')
+	sock.close()
